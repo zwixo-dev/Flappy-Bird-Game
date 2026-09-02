4bde65ae397eef93761064ec4114fda384cb535a
SaadDev <zwixozouine@gmail.com>
2026-09-02 23:36:40 +0100

    add trying_to_catch method to try catcing the bird by moving forward and backward

--- net_man.py
+++ net_man.py
@@ -9,22 +9,27 @@ class netMan(turtle.Turtle):
         super().__init__()
         self.shape(NET_MAN_PATH)
         self.penup()
-        self.setposition(x=-340, y=-400)
+        self.setposition(x=-360, y=-400)
+        # 
+        self.direction = 1 
 
     # control the movement of the bird catcher
-
-    def move_forward(self):
-        catcher_x_pos = self.xcor()
-        self.goto(x=catcher_x_pos+20, y=self.ycor())
-
-    def move_backward(self):
-        catcher_x_pos = self.xcor()
-        self.goto(x=catcher_x_pos-20, y=self.ycor())
-
-    # 
-    
+    def movement_path(self):
+        self.goto(x=self.xcor() + self.direction * 20, y=self.ycor())
 
 
+    def trying_to_catch(self):
+        """
+        Trying to catch the bird : when the xcor()>360 we set the direction to -1 and we move backward 
+        If the xcor() < -360  we move forward
+        """
+        print("=====================>",self.xcor())
+        if self.xcor() > 360:
+            self.direction = -1
+        elif self.xcor() <-360:
+            self.direction = 1
+
+        self.movement_path()    
 
 
 
