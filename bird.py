a31eb203ceab06d960321adc7caaa978bdb54b38
SaadDev <zwixozouine@gmail.com>
2026-09-02 21:30:31 +0100

    add right and left movement and update gravity func

--- bird.py
+++ bird.py
@@ -8,15 +8,23 @@ class Bird(turtle.Turtle):
     def __init__(self):
         super().__init__()
         self.shape(BIRD_IMG_PATH)
-        self.width(20)
         self.penup()
 
     # gravity control
     def gravity_control(self):
         current_y_pos = self.ycor()
-        self.goto(x=0, y=current_y_pos-5)
+        self.goto(x=self.xcor(), y=current_y_pos-8)
 
     # controle the movements
-    def up(self):
+    def move_up(self):
         current_y_pos = self.ycor()
-        self.goto(x=0, y=current_y_pos+30)
+        self.goto(x=self.xcor(), y=current_y_pos+30)
+
+    def move_left(self):
+        current_x_pos = self.xcor()
+        self.goto(x=current_x_pos-30, y=self.ycor())
+    def move_right(self):
+        current_x_pos = self.xcor()
+        self.goto(x=current_x_pos+30, y=self.ycor())
+
+    
