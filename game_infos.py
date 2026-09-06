844d4da5438d00fc1826c118662563e33fcec8dc
SaadDev <zwixozouine@gmail.com>
2026-09-06 20:59:29 +0100

    add class for game tutorial

---
+++ game_infos.py
@@ -0,0 +1,21 @@
+from tkinter import messagebox
+
+
+class Tutorial():
+    def __init__(self):
+        messagebox.showinfo(title="Welcome To The Flappy bird Game",
+                                message=
+                                    "This game was built fully with Python, so it can be a little bit slow.\n\n"
+                                    "Game Tutorial:\n"
+                                    "1. If you touch the left or right edges, you're going to die.\n"
+                                    "2. If you go to the top too much, your bird will die because there is no oxygen.\n"
+                                    "3. If you go to the bottom, the net man or his dog will catch you.\n\n"
+                                    "4. If you touch any evil bird, your bird gonna die."
+                                    "How To Play:\n"
+                                    "• Up key to move to the top\n"
+                                    "• Left key to move to the left\n"
+                                    "• Right key to move to the right"
+                                )
+
+
+
